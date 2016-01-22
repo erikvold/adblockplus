27494bb808deed6ac25eea1a8510f3507524c062
Erik Vold <erikvvold@gmail.com>
2016-01-21 17:05:53 -0800

    Adding some test files

---
+++ tests/filters/test-filter-class.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+var chai = require("chai");
+var expect = chai.expect;
+
+describe("Test Filter Class", function () {
+  it("Basic", function(done) {
+    expect(true).to.be.equal(true);
+  })
+});
+
